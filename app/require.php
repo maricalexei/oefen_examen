09a4dccc10ffa196cb8da24a5db00228df016174
Maric-Alexei Brouwer <329525@student.mboutrecht.nl>
2022-04-19 11:14:06 +0200

    Change: First commit with the base of mvc framework, Type:Addition, Why: This is the framework we will use

---
+++ app/require.php
@@ -0,0 +1,8 @@
+<?php
+// Require libraries from the folder libraries
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
+
+//    Instanatiate core class
+$init = new Core();
