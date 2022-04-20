791b23d42e636704354802783a56fdad4935d6b7
callofdeath <60213819+callofdeath@users.noreply.github.com>
2022-04-20 10:02:52 +0200

    Create connect_db.php

---
+++ Giovanni/connect_db.php
@@ -0,0 +1,7 @@
+<?php
+define("SERVERNAME" , "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DBNAME", "oefenexamen");
+
+$conn = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
