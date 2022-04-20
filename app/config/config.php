8797f77a6e6d619a4b92d1becaf1a95ac88f9dea
yusuf <321611@student.mboutrecht.com>
2022-04-20 16:25:45 +0200

    .

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+    <?php
+
+    //Database 
+    define('DB_HOST', 'localhost'); //Add your db host
+    define('DB_USER', 'root'); // Add your DB root
+    define('DB_PASS', ''); //Add your DB pass
+    define('DB_NAME', 'lending_desk'); //Add your DB Name
+
+    
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    
+    define('URLROOT', 'http://localhost/oefenexamen');
+
+  
+    define('SITENAME', 'Oefenexamen');
--- app/require.php
+++ app/require.php
@@ -3,6 +3,7 @@
 require_once 'libraries/Core.php';
 require_once 'libraries/Controller.php';
 require_once 'libraries/Database.php';
+require_once 'config/config.php';
 
 //    Instanatiate core class
 $init = new Core();
