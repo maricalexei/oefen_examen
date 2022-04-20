a9d80970aebf0da9a474005e67e6c2fa97f16d30
AronVLD <327156@student.mboutrecht.nl>
2022-04-20 13:33:07 +0200

    added first half of edit becouse this part now works

--- app/models/Item.php
+++ app/models/Item.php
@@ -5,11 +5,17 @@ class Item {
     public function __construct() {
         $this->db = new Database;
 	}
-
     public function getItems() {
         $this->db->query("SELECT * FROM item");
         $result = $this->db->resultSet();
         // var_dump($result);exit();
         return $result;
+    }
+    
+    public function deleteItems() {
+        $this->db->query("DELETE FROM `item` WHERE `item`.`id` = `{$id}`");
+        // $result = $this->db->resultSet();
+        // var_dump($result);exit();
+        // return $result;
     }
 }
